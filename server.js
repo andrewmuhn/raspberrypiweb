ddbaaf942098dea73a6c33ade69b6c84a7601d5f
andrew <andrewmuhn@gmail.com>
2023-10-08 12:47:39 -0700

    fix server.js syntax error

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require("fs");
 
-const const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  const count = readFileSync('./count.txt', 'utf-8');
-  console.log('count ', count);
+app.get("/", (req, res) => {
+  const count = readFileSync("./count.txt", "utf-8");
+  console.log("count ", count);
 
-  const newCount = parseInt(count) + 1
-  writeFileSync('./count.txt', newCount);
+  const newCount = parseInt(count) + 1;
+  writeFileSync("./count.txt", newCount);
 
   res.send(`
     <!DOCTYPE html>
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
 </html>
 
   `);
-})
+});
 
-app.listen(5000, () => console.log('http://localhost:5000/'));
+app.listen(5000, () => console.log("http://localhost:5000/"));
